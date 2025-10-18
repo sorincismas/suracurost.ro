d255ea078045532dda65962d7df599fcbc6e7716
Sorin Cismas <sorin.cismas@buonasera.ro>
2025-10-18 21:50:04 +0300

    Update save button label to 'Salvareee'

--- app/Views/Crm/angajati/form.php
+++ app/Views/Crm/angajati/form.php
@@ -68,7 +68,7 @@
                 Anulare
             </a>
             <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
-                <i class="fa-solid fa-save mr-2"></i> Salvare
+                <i class="fa-solid fa-save mr-2"></i> Salvareee
             </button>
         </div>
     </form>
