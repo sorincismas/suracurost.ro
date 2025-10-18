1a7a6482ac761d875f6fdd4e9daf9a89f8e58082
Sorin Cismas <sorin.cismas@buonasera.ro>
2025-10-18 22:05:30 +0300

    Change link text from 'Anulare' to 'Anulare1'

--- app/Views/Crm/angajati/form.php
+++ app/Views/Crm/angajati/form.php
@@ -65,7 +65,7 @@
 
         <div class="mt-8 text-right">
             <a href="<?= site_url('angajati') ?>" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md shadow-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
-                Anulare
+                Anulare1
             </a>
             <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
                 <i class="fa-solid fa-save mr-2"></i> Salvare
