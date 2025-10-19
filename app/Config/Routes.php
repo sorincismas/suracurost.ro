9a24cd9a7eced82f5880b014e3ec61d2477940f0
Sorin Cismas <sorin.cismas@buonasera.ro>
2025-10-19 09:47:23 +0000

    Add resource routes for Firme and Angajati controllers

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Adaugă aceste două linii:
+$routes->resource('firme', ['controller' => 'FirmeController']);
+$routes->resource('angajati', ['controller' => 'AngajatiController']);
