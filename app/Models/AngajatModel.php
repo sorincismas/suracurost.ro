bcba6567a25d5564840a76a8d45e141cb9de5811
sorincismas <sorin.cismas@buonasera.ro>
2025-10-20 15:07:31 +0300

    Add AngajatModel with validation rules and timestamps for employee management

---
+++ app/Models/AngajatModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AngajatModel extends Model
+{
+    protected $table = 'angajati';
+    protected $primaryKey = 'id';
+    
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    
+    protected $allowedFields = ['nume_prenume', 'cnp', 'id_firma', 'data_angajarii'];
+    
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    
+    protected $validationRules = [
+        'nume_prenume'   => 'required|min_length[3]|max_length[100]',
+        'cnp'            => 'required|exact_length[13]',
+        'id_firma'       => 'required|is_natural_no_zero',
+        'data_angajarii' => 'required|valid_date'
+    ];
+    
+    protected $validationMessages = [
+        'nume_prenume' => [
+            'required'    => 'Numele și prenumele sunt obligatorii',
+            'min_length'  => 'Numele și prenumele trebuie să aibă cel puțin 3 caractere',
+            'max_length'  => 'Numele și prenumele nu pot depăși 100 caractere'
+        ],
+        'cnp' => [
+            'required'      => 'CNP-ul este obligatoriu',
+            'exact_length' => 'CNP-ul trebuie să aibă exact 13 caractere'
+        ],
+        'id_firma' => [
+            'required'           => 'Firma este obligatorie',
+            'is_natural_no_zero' => 'Vă rugăm selectați o firmă validă'
+        ],
+        'data_angajarii' => [
+            'required'    => 'Data angajării este obligatorie',
+            'valid_date' => 'Vă rugăm introduceți o dată validă'
+        ]
+    ];
+}
